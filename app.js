1854081279f9355283e83e4a6f467191dae5b42e
tobiaspossetto <tobigpossetto@gmail.com>
2021-04-22 19:13:48 -0300

    pasado a jquery borrar

--- app.js
+++ app.js
@@ -1,78 +1,76 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     let listaProductos = $('#lista-productos');
-   let carritoHTML = $('#carritoHTML')
-   let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-
-
-//   $(carritoHTML).on('click', borrarProducto())
-   if (carrito[0]) {
-       
-       actualizarCarrito();
-   }
+    let carritoHTML = $('#carritoHTML')
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
 
 
-    
+    if (carrito[0]) {
+
+        actualizarCarrito();
+    }
 
 
 
 
 
 
-   listaProductos.click(function agregarProducto(e){
-       e.preventDefault();
-       if(e.target.classList.contains('interaction-añadir')){
-           const cardProducto = e.target.parentElement;
-           console.log(cardProducto);
-           obtenerDatosProducto(cardProducto);
-       }
-   })
 
-   function obtenerDatosProducto(cardProducto){
-       
-       
+
+
+
+    listaProductos.click(function agregarProducto(e) {
+        e.preventDefault();
+        if (e.target.classList.contains('interaction-añadir')) {
+            const cardProducto = e.target.parentElement;
+            console.log(cardProducto);
+            obtenerDatosProducto(cardProducto);
+        }
+    })
+
+    function obtenerDatosProducto(cardProducto) {
+
+
         const producto = {
-           nombre: $(cardProducto).children('#card-body').children('#card-title').text(),
-           imagen: $(cardProducto).children('a').children('img').attr('src'),
-           precio: $(cardProducto).children('#card-body').children('#precio').text(),
-           cantidad: 1,
-           id: $(cardProducto).children('#btn').data('id')
+            nombre: $(cardProducto).children('#card-body').children('#card-title').text(),
+            imagen: $(cardProducto).children('a').children('img').attr('src'),
+            precio: $(cardProducto).children('#card-body').children('#precio').text(),
+            cantidad: 1,
+            id: $(cardProducto).children('#btn').data('id')
 
-       
+
         }
 
-        console.log(producto)
+
         carrito.push(producto);
-        console.log(carrito)
+
 
         actualizarCarrito();
 
     }
-    
-    
-    function actualizarCarrito(){
-        console.log('EL CARRITO')
-        console.log(carrito)
-        
-        let contenidoCarrito=''
+
+
+    function actualizarCarrito() {
+
+
+        let contenidoCarrito = ''
         carritoHTML.html('');
-        console.log('--------------')
-        console.log(carrito[0].nombre)
 
-            
-            $(carrito).each(e =>{
-                console.log('--------------')
-                console.log(e.nombre)
-                let nombre = carrito[e].nombre;
-                let imagen =  carrito[e].imagen;
-                let precio =  carrito[e].precio;
-                let cantidad =  carrito[e].cantidad;
-                let id =  carrito[e].id;
 
-            
-                
-                contenidoCarrito += `
+
+
+        $(carrito).each(e => {
+
+            let nombre = carrito[e].nombre;
+            let imagen = carrito[e].imagen;
+            let precio = carrito[e].precio;
+            let cantidad = carrito[e].cantidad;
+            let id = carrito[e].id;
+
+
+
+            contenidoCarrito += `
                   <div class="producto" id='producto'> 
                   <div class="fila nombre "> 
                       <div class="img"> 
@@ -106,37 +104,50 @@ $(document).ready(function(){
                       <button type='button'  class='borrar-producto' data-id="${id}">QUITAR</button>
                   </div>
               </div>
+
+                <div class=" total ">
+                <p class="subtotal">
+                    Subtotal:
+                </p>
+                <p class="subtotal-valor">
+                    $35005
+                </p>
+        
+                <button type="button" class="btn btn-warning">Comprar</button>
+                </div>
+            </div>
+
               `
 
 
-            })
-            
-            //<a href="" class='borrar-producto'  data-id="${id}">QUITAR</a>
-           
+        })
+
+        //<a href="" class='borrar-producto'  data-id="${id}">QUITAR</a>
 
 
 
-            
-            carritoHTML.html(contenidoCarrito)
-            actualizarStorage()
-        
-    }   
-/*
-    function borrarProducto(e){
-        
 
-        if(e.target.classList.contains('borrar-producto')){
-            const id = e.target.attr('data-id');
 
-            carrito = carrito.filter(producto => producto.id !== id);
+        carritoHTML.html(contenidoCarrito)
+        actualizarStorage()
 
+    }
+
+    carritoHTML.click(function borrarProducto(e) {
+        console.log('borrarClick')
+        if (e.target.classList.contains('borrar-producto')) {
+            let id = e.target.getAttribute('data-id');
+            console.log(id)
+            carrito = carrito.filter(producto => producto.id != id);
+            console.log(carrito)
 
             actualizarCarrito()
             actualizarStorage()
         }
-    }
-*/
-    function actualizarStorage(){
+    })
+
+   
+    function actualizarStorage() {
         localStorage.setItem('carrito', JSON.stringify(carrito));
     }
 })
